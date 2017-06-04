16b5b965c9e15f27a1f165facd1752b06a592854
dmitryhits <dmitry.hits@gmail.com>
2017-06-04 10:46:49 +0200

    first commit test function for print3.py

---
+++ testprint3.py
@@ -0,0 +1,12 @@
+from print3 import print3
+print3(1,2,3)
+print3(1,2,3, sep='')
+print3(1,2,3, sep='...')
+print3(1, [2], (3,), sep='...')
+
+print3(4,5,6,sep='',end=' ')
+print(7,8,9)
+print3()
+
+import sys
+print3(1,2,3,sep='??',end='.\n',file=sys.stderr)
