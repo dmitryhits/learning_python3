783544d37177f858a4619f821425ccdc7310d141
dmitryhits <dmitry.hits@gmail.com>
2017-06-04 11:05:10 +0200

    changed get to pop and raise TypeError if extra keywords are present

--- print3.py
+++ print3.py
@@ -6,9 +6,10 @@ Call signature print3(*args, sep=' ', end='\n', file=sys.stdout
 import sys
 
 def print3(*args,**kargs):
-    sep = kargs.get('sep',' ')
-    end = kargs.get('end','\n')
-    file = kargs.get('file',sys.stdout)
+    sep = kargs.pop('sep',' ')
+    end = kargs.pop('end','\n')
+    file = kargs.pop('file',sys.stdout)
+    if kargs: raise TypeError('extra keywords: %s' % kargs)
     output = ''
     first = True
     for arg in args:
