4c29e4b73c3661e40c1c8178811067ce8e5d3249
Dmitry Alexandrovich Hits <dhits@phys.ethz.ch>
2017-07-13 20:20:12 +0200

    First commit of formats.py

---
+++ formats.py
@@ -0,0 +1,51 @@
+#!python
+"""
+File: formats.py (2.X and 3.X)
+Various specialized string formatting utilities.
+Test me with canned self-test or command-line arguments.
+To do: add parens for negative money. Add more features.
+"""
+
+def commas(N):
+    """
+    Format positive integer-like N for display with
+    commas between digit groupings: "xxx,yyy,zzz".
+    :param N:
+    :return:
+    """
+    digits = str(N)
+    assert digits.isdigit()
+    result = ''
+    while digits:
+        digits, last3 = digits[:-3], digits[-3:]
+        result = (last3 + ',' + result) if result else last3
+    return result
+
+def money(N, numwidth = 0, currency = '$'):
+    """
+    Format number N for display with commas, 2 decimal digits
+    leading $ and sign, and optional padding: "$  -xxx,yyy.zz".
+    numwidth = 0 for no space padding, currency = '' to omit the symbol
+    and non-ASCII for others (e.g. pound = u'\xA3 or u'\u00A3'
+    :param N:
+    :param numwidth:
+    :param currency:
+    :return:
+    """
+
+    sign = '-' if N < 0 else ''
+    N = abs(N)
+    whole = commas(int(N))
+    fract = ('%.2f' % N)[-2:]
+    number = '%s%s.%s' % (sign,whole, fract)
+    return '%s*%s' % (currency, numwidth, number)
+
+if __name__ == '__main__':
+    def selftest():
+        tests = 0, 1 # fails: -1, 1.23
+        tests += 12, 123, 1234, 12345, 123456, 1234567
+        tests += 2**32, 2**100
+        for test in tests:
+            print(commas(test))
+
+    
