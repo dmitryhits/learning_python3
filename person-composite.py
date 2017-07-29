8de443250cd6c5a0cd7e7f28030a76b27e1f85a6
Dmitry Alexandrovich Hits <dhits@phys.ethz.ch>
2017-07-29 15:59:10 +0200

    First commit of person-composite.py

---
+++ person-composite.py
@@ -0,0 +1,50 @@
+# person-composite.py
+
+class Person:
+    def __init__(self, name, job=None, pay=0):
+        self.name = name
+        self.job = job
+        self.pay = pay
+
+    def lastName(self):
+        return self.name.split()[-1]
+
+    def giveRaise(self, percent):
+        self.pay = int(self.pay*(1+percent))
+
+    def __repr__(self):
+        return '[Person: %s, %s]' % (self.name, self.pay)
+
+
+class Manager:
+    def __init__(self, name, pay):
+        self.person = Person(name, 'mgr', pay)
+
+    def giveRaise(self, percent, bonus=0.1):
+        self.person.giveRaise(percent+bonus)
+
+    def __getattr__(self, attr):
+        return getattr(self.person, attr)
+
+    def __repr__(self):
+        return str(self.person)
+
+
+
+if __name__ == '__main__':
+    bob = Person('Bob Smith')
+    sue = Person('Sue Jones', job='dev', pay=100000)
+    print(bob)
+    print(sue)
+    print(bob.lastName(), sue.lastName())
+    sue.giveRaise(0.10)
+    print(sue)
+    tom = Manager('Tom Jones', 50000)
+    print(tom)
+    print(tom.lastName())
+    tom.giveRaise(0.1)
+    print(tom)
+    print('-- All three --')
+    for obj in (sue, bob, tom):
+        obj.giveRaise(0.1)
+        print(obj)
