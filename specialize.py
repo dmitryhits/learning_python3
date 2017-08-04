3c95730c8e9cc34c3efb6c1ed9c29a2ce787ba61
dmitryhits <dmitry.hits@gmail.com>
2017-08-04 07:14:56 +0200

    "adding superclass interface demonstrator"

---
+++ specialize.py
@@ -0,0 +1,39 @@
+# specialize.py - demonstrates various ways to interface with superclass
+
+
+class Super:
+    def method(self):
+        print('In Super.method')
+    
+    def delegate(self):
+        self.action()
+
+
+class Inheritor(Super):
+    pass
+
+
+class Replacer(Super):
+    def method(self):
+        print('In Replacer.method')
+
+
+class Extender(Super):
+    def method(self):
+        print('Starting Extender.method')
+        Super.method(self)
+        print('Ending Extender.method')
+
+
+class Provider(Super):
+    def action(self):
+        print('In Provider.action')
+
+
+if __name__ == '__main__':
+    for klass in (Inheritor, Replacer, Extender):
+        print('\n' + klass.__name__ + '...')
+        klass().method()
+    print('\nProvider...')
+    x = Provider()
+    x.delegate()
