bb693f0c2ce34d02d8dff365e4bf5524ddd48d07
Dmitry Alexandrovich Hits <dhits@phys.ethz.ch>
2017-09-07 09:38:50 +0200

    starting with Set wrapper

---
+++ setwrapper.py
@@ -0,0 +1,22 @@
+# setwrapper.py
+
+
+class Set:
+    def __init__(self, value=[]):
+        self.data = []
+        self.concat(value)
+
+    def intersect(self, other):
+        res = []
+        for x in self.data:
+            if x in other:
+                res.append(x)
+        return Set(res)
+
+    def union(self, other):
+        res = self.data[:]
+        for x in other:
+            if x not in res:
+                res.append(x)
+        return Set(x)
+
